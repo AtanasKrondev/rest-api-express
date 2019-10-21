02fe3257b29cfa86316403b394a8bb4d6045fd0c
Atanas Krondev <krondev@gmail.com>
2019-10-21 14:53:54 +0300

    User model and basic CRUD

--- api/user.js
+++ api/user.js
@@ -4,12 +4,19 @@ const { User } = require('../models');
 router.get('/', (req, res, next) => {
     User.find().then(users => res.send(users)).catch(next)
 });
-router.post('/register', (req, res, next) => {
+
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+    User.findOne({ _id: id }).then(users => res.send(users)).catch(next)
+});
+
+router.post('/', (req, res, next) => {
     const { email, firstName, lastName, password } = req.body;
     User.create({ email, firstName, lastName, password })
         .then(user => res.send(user))
         .catch(next);
 });
+
 router.put('/:id', (req, res, next) => {
     const id = req.params.id;
     const { email, firstName, lastName, password } = req.body;
